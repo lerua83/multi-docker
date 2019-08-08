5ac7155b5546641168aa8b3791e7c19d08cfd0f4
Alvaro Leon Ruiz <alvaro.ruiz@internetstores.de>
2019-08-08 11:37:29 +0200

    changing title of the app

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
